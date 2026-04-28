1afd9e0cbc318077a4b7f1c7240c32e21a10cbba
Lars O <laoehler@davidson.edu>
2026-04-27 20:05:03 -0400

    fixed deployment issues with github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/tree-climber/",
+  base: "/Tree-Climber/",
   plugins: [react()]
 });
